7c972d8234d573821e21b4cecff2a37eac73c50a
Robert Sandor <sandorrobertk94@gmail.com>
2023-05-30 11:48:54 +0300

    small refactoring

--- backend/simplydash.go
+++ backend/simplydash.go
@@ -5,28 +5,31 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
-var CliArguments struct {
-	Host       string `help:"Set the host to listen on" default:"0.0.0.0"`
-	Port       int    `help:"Set the port to listen on" default:"8080"`
-	ConfigPath string `help:"Set path to config" type:"existingdir" default:"/app/config/"`
-	CachePath  string `help:"Set path to image cache" type:"existingdir" default:"/app/images/"`
-	Log        struct {
-		Level  string `help:"Set log level (debug / info / warn / error)" enum:"debug,info,warn,error" default:"info"`
-		Format string `help:"Set log format (console / json)" enum:"console,json" default:"console"`
-	} `embed:"" prefix:"log-"`
+type CliArguments struct {
+	Host       string       `help:"Set the host to listen on" default:"0.0.0.0"`
+	Port       int          `help:"Set the port to listen on" default:"8080"`
+	ConfigPath string       `help:"Set path to config"        default:"/app/config/" type:"existingdir"`
+	CachePath  string       `help:"Set path to image cache"   default:"/app/images/" type:"existingdir"`
+	Log        LogArguments `                                                                           embed:"" prefix:"log-"`
+}
+
+type LogArguments struct {
+	Level  string `help:"Set log level (debug / info / warn / error)" enum:"debug,info,warn,error" default:"info"`
+	Format string `help:"Set log format (console / json)"             enum:"console,json"          default:"console"`
 }
 
 func main() {
-	kong.Parse(&CliArguments)
-	setupLogging(CliArguments.Log.Level, CliArguments.Log.Format)
-	logrus.WithField("args", CliArguments).Info("starting...")
+	cliArgs := CliArguments{}
+	kong.Parse(&cliArgs)
+	setupLogging(cliArgs.Log)
+	logrus.WithField("args", cliArgs).Info("starting...")
 }
 
-func setupLogging(level, format string) {
-	logrusLevel, _ := logrus.ParseLevel(level)
+func setupLogging(logArgs LogArguments) {
+	logrusLevel, _ := logrus.ParseLevel(logArgs.Level)
 	logrus.SetLevel(logrusLevel)
 
-	if "json" == format {
+	if "json" == logArgs.Format {
 		logrus.SetFormatter(&logrus.JSONFormatter{})
 	} else {
 		logrus.SetFormatter(&logrus.TextFormatter{
