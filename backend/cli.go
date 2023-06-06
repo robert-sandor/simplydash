5cf8d91711f06135bc9f25e724a8c326ff902b02
Robert Sandor <sandorrobertk94@gmail.com>
2023-06-06 10:29:04 +0300

    add env vars to cli args

--- backend/cli.go
+++ backend/cli.go
@@ -6,16 +6,16 @@ import (
 )
 
 type CliArguments struct {
-	Host       string       `help:"Set the host to listen on" default:"0.0.0.0"`
-	Port       int          `help:"Set the port to listen on" default:"8080"`
-	ConfigPath string       `help:"Set path to config"        default:"/app/config/" type:"existingdir"`
-	CachePath  string       `help:"Set path to image cache"   default:"/app/images/" type:"existingdir"`
-	Log        LogArguments `                                                                           embed:"" prefix:"log-"`
+	Host       string       `help:"Set the host to listen on" default:"0.0.0.0"      env:"HOST"`
+	Port       int          `help:"Set the port to listen on" default:"8080"         env:"PORT"`
+	ConfigPath string       `help:"Set path to config"        default:"/app/config/" env:"CONFIG_PATH" type:"existingdir"`
+	ImagePath  string       `help:"Set path to image cache"   default:"/app/images/" env:"IMAGE_PATH"  type:"existingdir"`
+	Log        LogArguments `                                                                                             embed:"" prefix:"log-" envprefix:"LOG_"`
 }
 
 type LogArguments struct {
-	Level  string `help:"Set log level (debug / info / warn / error)" enum:"debug,info,warn,error" default:"info"`
-	Format string `help:"Set log format (console / json)"             enum:"console,json"          default:"console"`
+	Level  string `help:"Set log level (debug / info / warn / error)" enum:"debug,info,warn,error" default:"info"    env:"LEVEL"`
+	Format string `help:"Set log format (console / json)"             enum:"console,json"          default:"console" env:"FORMAT"`
 }
 
 func parseCliArgs() CliArguments {
