6ef789b46455cf6e8de5a071ad2bb375130f000f
Robert Sandor <sandorrobertk94@gmail.com>
2023-10-28 21:28:12 +0300

    small refactoring in main file

--- cmd/simplydash/main.go
+++ cmd/simplydash/main.go
@@ -3,28 +3,18 @@ package main
 import (
 	"context"
 	"fmt"
-	"log/slog"
-	"net/http"
-	"time"
-
-	"github.com/gorilla/websocket"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
-
 	"github.com/robert-sandor/simplydash/internal"
+	"log/slog"
+	"os"
 )
 
 func main() {
 	args := internal.GetArgs()
 	internal.SetupSlog(args)
 
-	slog.LogAttrs(context.Background(), slog.LevelDebug, "loading config",
-		slog.String("configFile", args.ConfigFile))
-
+	slog.LogAttrs(context.Background(), slog.LevelDebug, "loading config", slog.String("configFile", args.ConfigFile))
 	config, err := internal.GetConfig(args)
-	if err != nil {
-		slog.Error("invalid config", slog.Any("error", err))
-	}
+	logErrorAndExit(err, "invalid config")
 
 	healthCheckService := internal.NewHealthcheckService()
 	healthCheckService.Init()
@@ -37,85 +27,20 @@ func main() {
 	websocketServer := internal.NewWebsocketServer(appService)
 	websocketServer.Init()
 
+	slog.Debug("initializing image service")
 	imageService := internal.NewImageService(args.ImageCacheDir)
 
-	slog.Debug("initializing http server")
-	e := echo.New()
-	e.HideBanner = true
-	e.Use(middleware.Recover())
-	e.Use(middleware.CORS())
+	slog.Debug("initializing echo")
+	echo := internal.CreateEcho(args)
+	internal.SetupRouting(echo, websocketServer, imageService, config)
 
-	if args.AccessLogs {
-		e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
-			LogValuesFunc: func(_ echo.Context, values middleware.RequestLoggerValues) error {
-				slog.Info("http request", slog.Group("request",
-					slog.String("uri", values.URI),
-					slog.Any("latency", values.Latency),
-					slog.String("protocol", values.Protocol),
-					slog.String("remoteIp", values.RemoteIP),
-					slog.String("method", values.Method),
-					slog.Int("status", values.Status),
-					slog.String("contentLength", values.ContentLength),
-					slog.Int64("responseSize", values.ResponseSize),
-				))
-				return nil
-			},
-			LogLatency:       true,
-			LogProtocol:      true,
-			LogRemoteIP:      true,
-			LogMethod:        true,
-			LogURI:           true,
-			LogStatus:        true,
-			LogContentLength: true,
-			LogResponseSize:  true,
-		}))
-	}
+	err = echo.Start(fmt.Sprintf("%s:%s", args.Host, args.Port))
+	logErrorAndExit(err, "server shut down")
+}
 
-	e.Static("/", "./web/build")
-
-	upgrader := websocket.Upgrader{
-		CheckOrigin: func(r *http.Request) bool {
-			return true
-		},
-	}
-
-	e.GET("/ws", func(c echo.Context) error {
-		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-		if err != nil {
-			slog.Error("failed upgrade to websocket", slog.Any("error", err))
-			return err
-		}
-
-		websocketServer.Connect(time.Now().String(), ws)
-		return nil
-	})
-
-	e.GET("/image", func(c echo.Context) error {
-		filePath, err := imageService.Get(c.QueryParam("url"))
-		if err != nil {
-			slog.Error("image not found", slog.Any("error", err))
-			return c.NoContent(http.StatusNotFound)
-		}
-
-		return c.File(filePath)
-	})
-
-	e.GET("/settings", func(c echo.Context) error {
-		err := c.JSON(http.StatusOK, config.App)
-		if err != nil {
-			_ = c.NoContent(http.StatusInternalServerError)
-		}
-		return nil
-	})
-
-	e.GET("/timeout", func(c echo.Context) error {
-		time.Sleep(5 * time.Minute)
-		_ = c.NoContent(http.StatusOK)
-		return nil
-	})
-
-	err = e.Start(fmt.Sprintf("%s:%s", args.Host, args.Port))
+func logErrorAndExit(err error, msg string) {
 	if err != nil {
-		slog.Error("starting http server", slog.Any("error", err))
+		slog.Error(msg, "error", err)
+		os.Exit(1)
 	}
 }
---
+++ internal/echo.go
@@ -0,0 +1,96 @@
+package internal
+
+import (
+	"github.com/gorilla/websocket"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func SetupRouting(e *echo.Echo, websocketServer *WebsocketServer, imageService ImageService, config Config) {
+	e.Static("/", "./web/build")
+
+	e.GET("/ws", handleWebsocket(websocketServer))
+	e.GET("/image", getImage(imageService))
+	e.GET("/settings", getSettings(config))
+}
+
+func getSettings(config Config) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		err := c.JSON(http.StatusOK, config.App)
+		if err != nil {
+			_ = c.NoContent(http.StatusInternalServerError)
+		}
+		return nil
+	}
+}
+
+func getImage(imageService ImageService) func(c echo.Context) error {
+	return func(c echo.Context) error {
+		filePath, err := imageService.Get(c.QueryParam("url"))
+		if err != nil {
+			slog.Error("image not found", slog.Any("error", err))
+			return c.NoContent(http.StatusNotFound)
+		}
+
+		return c.File(filePath)
+	}
+}
+
+func handleWebsocket(websocketServer *WebsocketServer) func(c echo.Context) error {
+	upgrader := websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
+	}
+
+	return func(c echo.Context) error {
+		ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+		if err != nil {
+			slog.Error("failed upgrade to websocket", slog.Any("error", err))
+			return err
+		}
+
+		websocketServer.Connect(time.Now().String(), ws)
+		return nil
+	}
+}
+
+func CreateEcho(args Args) *echo.Echo {
+	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Recover())
+	e.Use(middleware.CORS())
+	if args.AccessLogs {
+		e.Use(loggingFunc())
+	}
+	return e
+}
+
+func loggingFunc() echo.MiddlewareFunc {
+	return middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogValuesFunc: func(_ echo.Context, values middleware.RequestLoggerValues) error {
+			slog.Info("http request", slog.Group("request",
+				slog.String("uri", values.URI),
+				slog.Any("latency", values.Latency),
+				slog.String("protocol", values.Protocol),
+				slog.String("remoteIp", values.RemoteIP),
+				slog.String("method", values.Method),
+				slog.Int("status", values.Status),
+				slog.String("contentLength", values.ContentLength),
+				slog.Int64("responseSize", values.ResponseSize),
+			))
+			return nil
+		},
+		LogLatency:       true,
+		LogProtocol:      true,
+		LogRemoteIP:      true,
+		LogMethod:        true,
+		LogURI:           true,
+		LogStatus:        true,
+		LogContentLength: true,
+		LogResponseSize:  true,
+	})
+}
